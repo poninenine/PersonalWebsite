897ba4d2b49f95681838b439c90dac3661b11ac7
poninenine <poninenine@hotmail.com>
2025-11-04 16:31:49 -0400

    Better homepage content width

--- my-website/app/page.tsx
+++ my-website/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center font-sans dark:bg-black">
       <Hero />
-      <main className="flex min-h-screen w-full max-w-3xl lg:max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl lg:max-w-5xl flex-col items-center justify-between py-32 md:px-16 bg-white dark:bg-black sm:items-start">
         <ProjectsGrid />
       </main>
     </div>
